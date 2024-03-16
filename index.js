1d3be54da8d5cf7565c27c461ac082013c46563d
JXS <113408314+Jishan76@users.noreply.github.com>
2024-03-16 18:35:52 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,38 +2,31 @@ const express = require('express');
 const ytSearch = require('yt-search');
 const ytdl = require('ytdl-core');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/video', async (req, res) => {
   const query = req.query.query;
-
+  
   if (!query) {
     return res.status(400).send('Missing query parameter');
   }
 
   try {
-    // Search for videos using yt-search
     const { videos } = await ytSearch(query);
-
+    
     if (!videos.length) {
       return res.status(404).send('No videos found');
     }
 
-    // Get the first video
     const firstVideo = videos[0];
-
-    // Get video details
     const videoInfo = await ytdl.getInfo(firstVideo.url);
-
-    // Get the highest quality video format
     const videoFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestvideo' });
+    
     if (!videoFormat) {
       return res.status(404).send('No video found');
     }
 
     const videoUrl = videoFormat.url;
-
-    // Redirect the user to the direct video URL
     res.redirect(videoUrl);
   } catch (error) {
     console.error('Error:', error);
@@ -43,34 +36,27 @@ app.get('/video', async (req, res) => {
 
 app.get('/audio', async (req, res) => {
   const query = req.query.query;
-
+  
   if (!query) {
     return res.status(400).send('Missing query parameter');
   }
 
   try {
-    // Search for videos using yt-search
     const { videos } = await ytSearch(query);
-
+    
     if (!videos.length) {
       return res.status(404).send('No videos found');
     }
 
-    // Get the first video
     const firstVideo = videos[0];
-
-    // Get video details
     const videoInfo = await ytdl.getInfo(firstVideo.url);
-
-    // Get the highest quality audio format
     const audioFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+    
     if (!audioFormat) {
       return res.status(404).send('No audio found');
     }
 
     const audioUrl = audioFormat.url;
-
-    // Redirect the user to the direct audio URL
     res.redirect(audioUrl);
   } catch (error) {
     console.error('Error:', error);
